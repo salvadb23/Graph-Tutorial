becd4803aef1bc64f8a4ca8583c5ef2681d07ad1
William Bogans <william.bogans@williambogans.dev>
2019-07-24 20:56:41 -0700

    challenge two done

--- Challenge/challenge_2.py
+++ Challenge/challenge_2.py
@@ -10,47 +10,6 @@ from collections import deque
 from challenge_1 import Graph, Vertex
 
 
-def parse_data():
-    vertices = open(argv[1], 'r')
-    graph_data = vertices.read().split()
-    vertices.close()
-    return graph_data
-
-
-def create_graph(graph_data):
-    is_graph = graph_data[0] is 'G'
-
-    graph = Graph()
-
-    for vertex in graph_data[1].split(','):
-        graph.addVertex(vertex)
-
-    counter = 0
-
-    for word in graph_data[2:]:
-        counter += 1
-        if is_graph:
-            graph.addEdge(word[3], word[1],
-                          word[5:].replace(')', ''))
-            graph.addEdge(word[1], word[3],
-                          word[5:].replace(')', ''))
-        else:
-            graph.addEdge(word[1], word[3],
-                          word[5:].replace(')', ''))
-
-    return graph, counter
-
-
-def print_graph(graph, counter):
-    print("# Vertices:", len(graph.getVertices()))
-    print("# Edges: ", counter, "\n")
-    print("Edge List:")
-    for v in graph:
-        for w in v.neighbors:
-            print("(%s ,%s, %s)" %
-                  (v.getId(), w.getId(), v.getEdgeWeight(w)))
-
-
 def shortest_path(graph):
     vertexOne = argv[2]
     vertexTwo = argv[3]
@@ -156,21 +115,9 @@ if __name__ == "__main__":
 
     def print_shortest_path(graph):
         path = shortest_path(graph)
-        for key in path.keys():
-            print(key, end=",")
-        print('\nNumber of edges: {}'.format(len(path)-1))
+        [print(key,end=",") for key in path.keys()]
+        print('\nNumber of edges in shortest path: {}'.format(len(path)-1))
 
-    # Almost done
-
-    def DFS(graph):
-        vertexOne = argv[3]
-        visited = {}
-        stack = [vertexOne]
-
-        while len(stack):
-            current_element = stack.pop()
-            if current_element not in visited:
-                visited[current_element] = True
-                for neighbor in graph[current_element.getID()].neighbors:
-                    if neighbor not in visited:
-                        stack.append(neighbor.getID())
+    graph_data = parse_data()
+    graph, counter = create_graph(graph_data)
+    print_shortest_path(graph)
