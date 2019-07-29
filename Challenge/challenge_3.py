8f2f34602c28aa4885724193c04448a15bb6c606
William Bogans <william.bogans@williambogans.dev>
2019-07-28 23:40:48 -0700

    finished challenge 3

--- Challenge/challenge_3.py
+++ Challenge/challenge_3.py
@@ -4,22 +4,29 @@ from challenge_1 import Graph, Vertex, create_graph, parse_data
 '''Ask soul why are these giving different outputs'''
 
 
-def rescursive_dfs(graph, v):
-    vertexObj = graph.vertList[v]
-    result = [vertexObj]
+def DFS_recursive(self, v, v2):
+    """searches the graph to see if there is a path between two vertices using DFS"""
+    vertexObj = self.vertList[v]
     visited = {}
+    visited[vertexObj.getId()] = True
 
     def dfs(vertex):
         visited[vertex.getId()] = True
         for neighbor in vertex.neighbors:
-            if neighbor not in visited:
+            if neighbor.getId() not in visited:
                 dfs(neighbor)
+
     dfs(vertexObj)
 
-    return list(visited.keys())
+    path = list(visited.keys())
+    end_path = path.index(v2) + 1
+
+    is_path = v2 in path
+
+    return (is_path, path[:end_path])
 
 
 graph_data = parse_data()
 graph = create_graph(graph_data)[0]
-result = rescursive_dfs(graph, '1')
+result = DFS_recursive(graph, '1', '5')
 print(result)
